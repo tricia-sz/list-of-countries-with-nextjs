0f573fceeade877060c7a55f8c80f4ed7afd6e2a
Patrícia Souza <patricia.sz.br@gmail.com>
2023-10-07 19:51:50 -0300

    Fetch and map api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-export default function Home() {
+import { Country } from "@/utils/types"
+
+async function fetchAllCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all")
+
+  return response.json()
+}
+
+export default async function Home() {
+  const countries = await fetchAllCountries()
+
   return (
-    <main>
-      <section className="w-full flex container">
-        <h1>Hello</h1>
-      </section>
-    </main>
+    <section className="w-full flex container">
+      {countries.map((country: Country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
+    </section>
   )
 }
---
+++ src/utils/types.ts
@@ -0,0 +1,6 @@
+export type Country = {
+  map(arg0: (item: any) => import("react").JSX.Element): import("react").ReactNode;
+  name: {
+    common: string;
+  }
+}
